8273451351ef4e9f060d807a07f4f2e826c1e30e
kooqooo <koogmlcks@gmail.com>
2026-06-21 17:59:06 +0900

    feat: exam_level 테이블 추가

---
+++ src/main/java/kr/ai/ket/entity/ExamLevel.java
@@ -0,0 +1,23 @@
+package kr.ai.ket.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(
+        name = "exam_level",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_exam_level_level", columnNames = "level")
+        }
+)
+public class ExamLevel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int level;
+
+}
